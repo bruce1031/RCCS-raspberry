39cd35188a95ce88f9c7259e530793c2b2f63256
bruce1031 <88383840+bruce1031@users.noreply.github.com>
2023-03-17 10:19:09 +0800

    在程式裡新增log顯示錯誤

--- start.py
+++ start.py
@@ -5,28 +5,31 @@ import threading
 #001使用
 def log(title , log):
     '''title輸入標題,log有錯誤訊息打入(無請打入None)'''
-    f = open("serverlog.txt" , "a+")
-    if log == None:
-        f.write(f'{time()}\n Massage:{title}\n')
-    else:
-        f.write(f'{time()}\n Massage:{title}\n [ERROR] {log}')
+    with open("serverlog.txt" , "a+") as f:
+        if log == None:
+            f.write(f'{time()}\nMassage:{title}\n ------------------------------\n')
+        else:
+            f.write(f'{time()}\n Massage:{title}\n [ERROR] {log}\n------------------------------\n')
 
 
 def time():
     today=datetime.datetime.today()
-    today = today.strftime("time:%Y/%m/%d %H:%M:%S")
+    today = today.strftime("Time:%Y/%m/%d %H:%M:%S")
     print(today)
     return today
 def a():
-    os.system('sudo python3 /home/pi/001/servicegoogle1.1.py')
-
+    try:
+        os.system('sudo python3 /home/pi/001/servicegoogle1.1.py')
+    except:
+        log('開啟servicegoogle1.1.py錯誤', e)
     
 def b():
-    os.system('sudo nginx service start')
-    os.system('ffmpeg -i /dev/video0 -f flv rtmp://172.23.121.179:1935/live/test')
-
-time()
-
+    try:
+        os.system('sudo nginx service start')
+        os.system('ffmpeg -i /dev/video0 -f flv rtmp://172.23.121.179:1935/live/test')
+    except:
+        log('影像串流發生問題', e)
+log('successfully executed', None)
 
 try:
     point1 = threading.Thread(target = a)
