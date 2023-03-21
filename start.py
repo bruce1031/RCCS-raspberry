802e54e0dca2545d973c4b1bea84f9b5e4ad60cb
bruce1031 <88383840+bruce1031@users.noreply.github.com>
2023-03-21 16:16:20 +0800

    start.py修正路徑，改成絕對路徑

--- start.py
+++ start.py
@@ -5,22 +5,22 @@ import threading
 #001使用
 def log(title , log):
     '''title輸入標題,log有錯誤訊息打入(無請打入None)'''
-    with open("serverlog.txt" , "a+") as f:
+    with open("/home/pi/serverlog.txt" , "a+") as f:
         if log == None:
             f.write(f'{time()}\nMassage:{title}\n ------------------------------\n')
         else:
             f.write(f'{time()}\n Massage:{title}\n [ERROR] {log}\n------------------------------\n')
-
-
 def time():
     today=datetime.datetime.today()
     today = today.strftime("Time:%Y/%m/%d %H:%M:%S")
     print(today)
     return today
+
 def a():
     try:
         os.system('sudo python3 /home/pi/001/servicegoogle1.1.py')
     except:
+
         log('開啟servicegoogle1.1.py錯誤', e)
     
 def b():
@@ -28,6 +28,7 @@ def b():
         os.system('sudo nginx service start')
         os.system('ffmpeg -i /dev/video0 -f flv rtmp://172.23.121.179:1935/live/test')
     except:
+
         log('影像串流發生問題', e)
 
 try:
