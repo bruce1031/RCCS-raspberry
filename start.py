5262712d6b94378cce6c5944fcf83b93066640ea
bruce1031 <88383840+bruce1031@users.noreply.github.com>
2023-03-31 18:05:41 +0800

    修改固定路徑

--- start.py
+++ start.py
@@ -5,7 +5,7 @@ import threading
 #001使用
 def log(title , log):
     '''title輸入標題,log有錯誤訊息打入(無請打入None)'''
-    with open("/home/pi/serverlog.txt" , "a+") as f:
+    with open("/home/pi/RCCS-raspberry/serverlog.txt" , "a+") as f:
         if log == None:
             f.write(f'{time()}\nMassage:{title}\n ------------------------------\n')
         else:
@@ -18,9 +18,8 @@ def time():
 
 def a():
     try:
-        os.system('sudo python3 /home/pi/001/servicegoogle1.1.py')
+        os.system('sudo python3 /home/pi/RCCS-raspberry/servicegoogle1.1.py')
     except:
-
         log('開啟servicegoogle1.1.py錯誤', e)
     
 def b():
